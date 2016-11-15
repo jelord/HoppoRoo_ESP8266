1791ed4cbac0c1996e389439ec0e450e4d20e26e
jelord <jakel92@comcast.net>
2016-11-14 21:59:14 -0700

    update doors.lua
    FILE

--- doors.lua
+++ doors.lua
@@ -1,45 +1,46 @@
 srv=net.createServer(net.TCP) 
 
-gpio.mode(0,gpio.INPUT,gpio.FLOAT)
-gpio.mode(6,gpio.INPUT,gpio.FLOAT)
-gpio.mode(7,gpio.INPUT,gpio.FLOAT)
-gpio.mode(8,gpio.INPUT,gpio.FLOAT)
-gpio.write(0,gpio.LOW)
-gpio.write(6,gpio.LOW)
-gpio.write(7,gpio.LOW)
-gpio.write(8,gpio.LOW)
+gpio.mode(5,gpio.INPUT,gpio.PULLUP)
+gpio.mode(6,gpio.INPUT,gpio.PULLUP)
+gpio.mode(7,gpio.INPUT,gpio.PULLUP)
+gpio.mode(8,gpio.INPUT,gpio.FLOAT) --pulldown
+
+gpio.mode(0,gpio.OUTPUT) --yellow
+gpio.mode(1,gpio.OUTPUT) --blue
+gpio.mode(2,gpio.OUTPUT) --green
+gpio.mode(3,gpio.OUTPUT) --red
 
 srv:listen(8080,function(conn) 
 	conn:on("receive",function(conn,payload) 
 	print(node.heap())
 	
-	-- this is for button A --> gpio 16 on the board
-	if gpio.read(0)==1 
-		then buttonA="1" 
-	else buttonA="0" 
+	-- this is for button A --> gpio 14 on the board
+	if gpio.read(5)==1 
+		then gpio.write(3,gpio.HIGH) 
+	else gpio.write(3,gpio.LOW) 
 	end 
 	
 	-- this is for button B --> gpio 12 on the board
 	if gpio.read(6)==1 
-		then buttonB="1" 
-	else buttonB="0" 
+		then gpio.write(2,gpio.HIGH) 
+	else gpio.write(2,gpio.LOW) 
 	end 
 	
 	-- this is for Button C --> gpio 13 on the board
 	if gpio.read(7)==1 
-		then buttonC="1" 
-	else buttonC="0" 
+		then gpio.write(1,gpio.HIGH)
+	else gpio.write(1,gpio.HIGH)
 	end 
 	
 	-- this is for Button D --> gpio 15 on the board
 	if gpio.read(8)==1 
-		then buttonD="1" 
-	else buttonD="0" 
+		then gpio.write(0,gpio.HIGH)
+	else gpio.write(0,gpio.HIGH) 
 	end 
-	str = "jsonCallBack({\"buttonA\": \"" .. buttonA .. "\",\
-		\"buttonB\" : \"" .. buttonB .. "\",\
-		\"buttonC\" : \"" .. buttonC .. "\",\
-		\"buttonD\" : \"" .. buttonD .. "\"})"
+	str = "jsonCallBack({\"buttonA\": \"" .. gpio.read(3)  .. "\",\
+		\"buttonB\" : \"" .. gpio.read(2) .. "\",\
+		\"buttonC\" : \"" ..  gpio.read(1) .. "\",\
+		\"buttonD\" : \"" .. gpio.read(0) .. "\"})"
 	conn:send(str)
 	conn:close() 
 	end)
